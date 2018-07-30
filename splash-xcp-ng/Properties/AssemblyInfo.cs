21dd0b43b73af5d5e4e0015cb67b25e563407bda
cocoon <cocoon@users.noreply.github.com>
2018-07-30 08:30:11 +0200

    add AssemblyInformationalVersion

--- splash-xcp-ng/Properties/AssemblyInfo.cs
+++ splash-xcp-ng/Properties/AssemblyInfo.cs
@@ -16,3 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("cd563857-6159-4415-b7b2-a21991f572e3")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0 RC1")]
