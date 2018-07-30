d822131e0faa74f646deabc677d56e541108a780
cocoon <cocoon@wolke7.net>
2018-07-30 08:55:03 +0200

    rename form and assembly, add safty counter

--- splash-xcp-ng/Program.cs
+++ splash-xcp-ng/Program.cs
@@ -10,7 +10,7 @@ namespace splash_xcp_ng
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SplashForm());
         }
     }
 }
