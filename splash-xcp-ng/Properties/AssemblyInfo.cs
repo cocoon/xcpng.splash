145dcddf37569a99d09a222b27422f3d9fd66e5c
cocoon <cocoon@users.noreply.github.com>
2018-08-12 08:27:25 +0200

    prepare for branding

--- splash-xcp-ng/Properties/AssemblyInfo.cs
+++ splash-xcp-ng/Properties/AssemblyInfo.cs
@@ -3,17 +3,17 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 //FixMe: insert Branding
-[assembly: AssemblyTitle("splash-xcp-ng")]
+[assembly: AssemblyTitle("[XenCenter] Splash")]
 [assembly: AssemblyDescription("XCP-ng Center Launcher")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("XCP-ng.org")]
-[assembly: AssemblyProduct("splash-xcp-ng")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("[Citrix]")]
+[assembly: AssemblyProduct("[XenCenter]")]
+[assembly: AssemblyCopyright("[BRANDING_COPYRIGHT]")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("cd563857-6159-4415-b7b2-a21991f572e3")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0 RC1")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0000")]
+[assembly: AssemblyInformationalVersion("[BRANDING_PRODUCT_VERSION]")]
